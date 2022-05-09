c97eadff5b1dba6c8e8bab9119b8ccb6c65f9c12
AlexsandroXavier <eng.alexavier@gmail.com>
2022-05-09 15:09:24 -0300

    Revisando codigo

---
+++ js/main.js
@@ -0,0 +1,28 @@
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace (nome, novo_nome)
+}
+
+alert(soma(5, 10));
+alert(setReplace("vai Japão", "japão", "Brasil",));
+
+
+
+/*var lista = ["maça", "uva", "laranja"];
+lista.push("banana");
+lista.pop();
+console.log  (lista.reverse());
+alert (lista[0]);
+var nome = "Alex Xavier";
+var idade = 27;
+var idade2 = 8;
+var frase = "Bahia é o melhor time do mundo!";
+alert(nome + " tem " + idade + " anos");
+alert(idade + idade2);
+console.log (idade);
+console.log (nome);
+console.log (frase.replace("Bahia", "Palmeiras").toUpperCase());
+alert (frase.replace("Bahia", "Palmeiras"));*/
